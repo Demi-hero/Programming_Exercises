5e521e936eb21af4b0576fa06078738eb5368021
Nate <scott.pearce3@gmail.com>
2018-10-23 11:38:09 +0100

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 TODO 
-- Imitate the layout of the pdf output 
 - Handel a user input of = 
 - Handel a user input of <
 - Handel a user input of > 
 - Handel incorrect input
-- let the user know it figure out its cheating
+- let the user know it figured out its cheating
 """
-# Declaring all the important variables to begin with just to get it done
+# Declaring all the important variables to prevent magic numbers
 guess = 50 
 max_upper_bound = 100
 upper_bound = 100
@@ -23,18 +22,19 @@ def val_check(var1, var2) :
     else:
         return False 
     
-# lets me shorthand out formula for deminishing returns   
+# A formula for deminishing returns after each guess  
 def deminishing_returns():
     x = int(max_upper_bound/2**counter)
     if x == 0:
         x = 1
     return x
-#lets me easily reset the game if the user gives me bad data   
+#Tells the user I know they cheated
 def cheater():
     print("Those steps take us beyond one of our bounds")
     print("This means you have given inconsitent answers.")
     print("I don't work if you do that. Thanks for playing")
-# lets me quickly give the user a guess and asks for their response
+
+# gives the user a guess and asks for their response
 def user_input_request():
     print('Is your number: {guess}?'.format(guess=guess))
     return input("If so let me know with =. Otherwise is it greater than(>) or less than(<).\n")
@@ -44,7 +44,7 @@ print("Think of a number between 1 and 100.")
 print("Using the power of Maths I shall figure it out.")
 user_response = user_input_request()
 
-
+# The important part 
 while check == True:
         # lets the user know the computer has guessed their number 
         if user_response == "=" :
@@ -74,5 +74,6 @@ while check == True:
                 break
             user_response = user_input_request()
         else:
+            # what is given when they input bad entry data
             print ("Thats not quite right please input =,< or, >")
             break       
